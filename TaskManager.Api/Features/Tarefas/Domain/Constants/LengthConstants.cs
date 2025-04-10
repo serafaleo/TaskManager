7a5702f7ea06da8ccc43822f067ae0283d49eae7
Leonardo <serafa.leo@proton.me>
2025-04-09 22:09:53 -0300

    Adicionado validator para o model Tarefa

---
+++ TaskManager.Api/Features/Tarefas/Domain/Constants/LengthConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskManager.Api.Features.Tarefas.Domain.Constants;
+
+public static class LengthConstants
+{
+    public const int TITULO_MAX_LENGTH = 100;
+}
--- TaskManager.Api/Features/Tarefas/Domain/Models/Tarefa.cs
+++ TaskManager.Api/Features/Tarefas/Domain/Models/Tarefa.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using TaskManager.Api.Core.Helpers.ExtensionMethods;
 using TaskManager.Api.Core.Models;
+using TaskManager.Api.Features.Tarefas.Domain.Constants;
 using TaskManager.Api.Features.Tarefas.Domain.Enums;
 
 namespace TaskManager.Api.Features.Tarefas.Domain.Models;
@@ -22,7 +24,27 @@ public class TarefaConfiguration : IEntityTypeConfiguration<Tarefa>
         builder.BaseConfiguration();
 
         builder.Property(tarefa => tarefa.Titulo)
-            .HasMaxLength(100)
+            .HasMaxLength(LengthConstants.TITULO_MAX_LENGTH)
             .IsRequired();
+    }
+}
+
+public class TarefaValidator : AbstractValidator<Tarefa>
+{
+    public TarefaValidator()
+    {
+        RuleFor(tarefa => tarefa.Titulo)
+            .NotEmpty().WithMessage("Título deve ser preenchido.")
+            .MaximumLength(LengthConstants.TITULO_MAX_LENGTH).WithMessage($"Título deve ter no máximo {LengthConstants.TITULO_MAX_LENGTH} caracteres.");
+
+        When(tarefa => tarefa.DataConclusao is not null, () =>
+        {
+            RuleFor(tarefa => tarefa.DataConclusao)
+                .Must((tarefa, dataConclusao) => dataConclusao > tarefa.DataConclusao)
+                .WithMessage("Data de conclusão deve ser posterior à data de criação.");
+        });
+
+        RuleFor(tarefa => tarefa.Status)
+            .IsInEnum().WithMessage("Status inválido.");
     }
 }
